5686018976e3842d950d7422bd89867e7d4d093f
ryandoherty1 <37849271+ryandoherty1@users.noreply.github.com>
2019-01-24 14:42:07 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+var img = "0"
+
+var rain = "0"
+	
+// "el" stands for the "element" that was just clicked
+
+// "el" stands for the "element" that was just clicked
+function removeColor(el){
+	if(rain = "0") {
+	document.getElementById("rainbow8s").classList.remove("rainbow8s");
+	document.getElementById("rainbow8s").classList.add("bandw2s");
+	document.getElementById("rainbow10s").classList.remove("rainbow10s");
+	document.getElementById("rainbow10s").classList.add("bandw5s");
+	document.getElementById("rainbow12s").classList.remove("rainbow12s");
+	document.getElementById("rainbow12s").classList.add("bandw7s");
+	rain = "1";
+	}
+	else {
+	document.getElementById("rainbow8s").classList.add("rainbow8s");
+	document.getElementById("rainbow8s").classList.remove("bandw2s");
+	document.getElementById("rainbow10s").classList.add("rainbow10s");
+	document.getElementById("rainbow10s").classList.remove("bandw5s");
+	document.getElementById("rainbow12s").classList.add("rainbow12s");
+	document.getElementById("rainbow12s").classList.remove("bandw7s");
+	rain = "0";
+	}
+}
+
+function changeImg(el){
+	if(img == "0") {
+	document.getElementById("rainbow").src="image/donut.gif";
+	document.getElementById("blank").src="image/spherical.gif";
+	document.getElementById("hands").src="image/paper-ball.gif";
+	img = "1";
+	}
+	else if(img == "1"){
+	document.getElementById("rainbow").src="image/sun.gif";
+	document.getElementById("blank").src="image/spires.gif";
+	document.getElementById("hands").src="image/spiral.gif";
+	img = "2";
+	}
+	else{
+	document.getElementById("rainbow").src="image/rainbow.gif";
+	document.getElementById("blank").src="";
+	document.getElementById("hands").src="image/hands.gif";
+	img = "0";
+	}
+}
