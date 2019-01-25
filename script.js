ffa689c5eb44dc3a4a0e28c9a66551f8d8a764c5
ryandoherty1 <37849271+ryandoherty1@users.noreply.github.com>
2019-01-25 15:18:07 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 // https://www.w3schools.com/jsref/met_document_getelementbyid.asp
 
 var img = "0"
-
 var rain = "0"
+var back = "0"
+var brk = "0"
 	
 // "el" stands for the "element" that was just clicked
 
 // "el" stands for the "element" that was just clicked
 function removeColor(el){
-	if(rain = "0") {
+	if(rain == "0") {
 	document.getElementById("rainbow8s").classList.remove("rainbow8s");
 	document.getElementById("rainbow8s").classList.add("bandw2s");
 	document.getElementById("rainbow10s").classList.remove("rainbow10s");
@@ -18,7 +19,7 @@ function removeColor(el){
 	document.getElementById("rainbow12s").classList.add("bandw7s");
 	rain = "1";
 	}
-	else {
+	else{
 	document.getElementById("rainbow8s").classList.add("rainbow8s");
 	document.getElementById("rainbow8s").classList.remove("bandw2s");
 	document.getElementById("rainbow10s").classList.add("rainbow10s");
@@ -49,3 +50,57 @@ function changeImg(el){
 	img = "0";
 	}
 }
+
+function remaddBack(el) {
+	if(back == "0") {
+		document.getElementById("rainbow8s").classList.remove("rainbow8s");
+		document.getElementById("rainbow8s").classList.remove("bandw2s");
+		document.getElementById("rainbow10s").classList.remove("rainbow10s");
+		document.getElementById("rainbow10s").classList.remove("bandw5s");
+		document.getElementById("rainbow12s").classList.remove("rainbow12s");
+		document.getElementById("rainbow12s").classList.remove("bandw7s");
+		document.body.style.backgroundImage = "url('image/repeat.gif')";
+		document.getElementById("rainbow").src="";
+		document.getElementById("blank").src="";
+		document.getElementById("hands").src="";
+		back = "1";
+	}
+	else if(back == "1") {
+		document.body.style.backgroundImage = "url('image/square.gif')";
+		back = "2";
+	}
+	else {
+		document.getElementById("rainbow8s").classList.add("rainbow8s");
+		document.getElementById("rainbow10s").classList.add("rainbow10s");
+		document.getElementById("rainbow12s").classList.add("rainbow12s");
+		document.getElementById("rainbow").src="image/rainbow.gif";
+		document.getElementById("blank").src="";
+		document.getElementById("hands").src="image/hands.gif";
+		document.body.style.backgroundImage = "url('image/trippy.gif')";
+		back = "0";
+	}
+}
+
+function Broken(el) {
+	document.getElementById("rainbow8s").classList.remove("rainbow8s");
+	document.getElementById("rainbow8s").classList.remove("bandw2s");
+	document.getElementById("rainbow10s").classList.remove("rainbow10s");
+	document.getElementById("rainbow10s").classList.remove("bandw5s");
+	document.getElementById("rainbow12s").classList.remove("rainbow12s");
+	document.getElementById("rainbow12s").classList.remove("bandw7s");
+	document.getElementById("rainbow").src="";
+	document.getElementById("blank").src="";
+	document.getElementById("hands").src="";
+	document.body.style.backgroundImage = "url('image/trippy.gif')";
+	sleep(5000);
+	document.getElementById("rainbow8s").classList.add("rainbow8s");
+	document.getElementById("rainbow10s").classList.add("rainbow10s");
+	document.getElementById("rainbow12s").classList.add("rainbow12s");
+	document.getElementById("rainbow").src="image/rainbow.gif";
+	document.getElementById("blank").src="";
+	document.getElementById("hands").src="image/hands.gif";
+	document.body.style.backgroundImage = "url('image/trippy.gif')";
+}
+	
+		
+		
